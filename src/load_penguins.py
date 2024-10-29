9032478a2abc019e49f1f25fca1b803c9af3ec65
Daniel Incicau <daniel.incicau@gmail.com>
2024-10-29 18:49:49 +0100

    Write features and labels separately

--- src/load_penguins.py
+++ src/load_penguins.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 from pathlib import Path
 
+from sklearn.preprocessing import StandardScaler
+
 
 def main():
     # Create argument parser
@@ -18,9 +20,26 @@ def main():
     script_dir = Path(__file__).parent.resolve()
     penguins = pd.read_csv(script_dir / '..' / 'data' / 'dataset.csv')
 
+    # Prepare features
+    features = penguins[['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex']]
+    features = features.dropna()
+    features['sex'] = pd.factorize(features['sex'])[0]
+
+
+    scaler = StandardScaler()
+    numeric_features = features.drop(columns=['sex'])
+    numeric_features_scaled = scaler.fit_transform(numeric_features)
+    features_scaled_df = pd.DataFrame(numeric_features_scaled, columns=numeric_features.columns)
+    features_scaled_df['sex'] = features['sex'].values
+
+    # Prepare labels
+    labels_df = penguins[['species']].copy()
+    labels_df['species'] = pd.factorize(labels_df['species'])[0]
+    labels_df.columns = ['label']
+
     # Write to disk
-    output_path = Path(args.output_dir) / f'{args.name}.csv'
-    penguins.to_csv(output_path, index=False)
+    features_scaled_df.to_csv(Path(args.output_dir) / f'{args.name}.features.csv', index_label='id')
+    labels_df.to_csv(Path(args.output_dir) / f'{args.name}.labels.csv', index_label='id')
 
 
 if __name__ == "__main__":
